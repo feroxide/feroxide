13acc3385cacd2711733ecc664e2bc1b5dbf97cf
Cxarli <10348289+Cxarli@users.noreply.github.com>
2017-03-11 22:59:50 +0100

    Fix build error on stable branch

--- src/molecules.rs
+++ src/molecules.rs
@@ -1,14 +1,14 @@
 use atoms::*;
 use molecule::*;
 
-pub const WATER: &'static Molecule = &Molecule {
+pub const WATER: &'static Molecule<'static> = &Molecule {
     compounds: &[
         MoleculeCompound { atom: HYDROGEN, amount: 2 },
         MoleculeCompound { atom: OXYGEN, amount: 1 }
     ]
 };
 
-pub const TABLE_SALT: &'static Molecule = &Molecule {
+pub const TABLE_SALT: &'static Molecule<'static> = &Molecule {
     compounds: &[
         MoleculeCompound { atom: SODIUM, amount: 1 },
         MoleculeCompound { atom: CHLORINE, amount: 1 }
