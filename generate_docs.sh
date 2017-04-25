0702557f34713facb0800409c04a36bcb8a18a99
Cxarli <10348289+Cxarli@users.noreply.github.com>
2017-04-26 01:11:41 +0200

    Added Reaction* to SR docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-## Rust-docs
-
-# Clean old files
-rm -R ./rust-docs
-
-# Generate docs
-cargo doc --all --no-deps --release --all-features --color always
-
-# Copy docs to root directory
-cp -R ./target/doc ./rust-docs
-
-
 ## Latex docs
 cd docs
 mkdir -p build
@@ -23,3 +11,24 @@ mv build/*.pdf .
 rm -r build
 
 cd ..
+
+
+while true; do
+    read -p "Do you want to regenerate Rust docs too? [Yn]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+    esac
+done
+
+
+## Rust-docs
+
+# Clean old files
+rm -R ./rust-docs
+
+# Generate docs
+cargo doc --all --no-deps --release --all-features --color always
+
+# Copy docs to root directory
+cp -R ./target/doc ./rust-docs
